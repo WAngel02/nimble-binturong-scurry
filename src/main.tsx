5d79e04bc7466b6866aa5c89ae20c87428d75b6e
[dyad] <angelwalcott02@gmail.com>
2025-08-20 21:30:42 +0200

    [dyad] Corregir error de importación de CSS del calendario - wrote 3 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
